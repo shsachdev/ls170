f96337ec5809622834769ba190bc25725ec599a7
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-03 21:56:00 -0800

    doing some problem solving to refresh the mind

---
+++ substrings_q.rb
@@ -0,0 +1,14 @@
+def palindromes(str)
+end
+
+
+puts palindromes('abcd') == []
+puts palindromes('madam') == ['madam', 'ada']
+puts palindromes('hello-madam-did-madam-goodbye') == [
+  'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
+  'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
+  '-madam-', 'madam', 'ada', 'oo'
+]
+puts palindromes('knitting cassettes') == [
+  'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
+]
