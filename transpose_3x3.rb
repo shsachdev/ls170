6a2692399607074c84031744d5470ec0f5999450
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-03 23:00:10 -0800

    3x3 matrix has been transposed successfully

--- transpose_3x3.rb
+++ transpose_3x3.rb
@@ -1,6 +1,12 @@
 def transpose(mtx)
   copy_mtx = mtx.clone
-
+  new_mtx = [[],[],[]]
+  copy_mtx.each do |sub_arr|
+    sub_arr.each_with_index do |elem, index|
+      new_mtx[index] << elem
+    end
+  end
+  new_mtx
 end
 
 
@@ -16,4 +22,9 @@ p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
 p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
 
 
-# The transpose of a matrix is a new matrix whose rows are the columns of the original. 
+# The transpose of a matrix is a new matrix whose rows are the columns of the original.
+
+
+# Algorithm:
+
+#
