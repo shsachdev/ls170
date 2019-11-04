4b24dd3b56e957fa57a659305097ce67e9462822
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-03 22:16:37 -0800

    i am a god

--- substrings_q.rb
+++ substrings_q.rb
@@ -1,5 +1,30 @@
-def palindromes(str)
+def substrings(string)
+  chars_array = string.chars
+  chars_size = chars_array.size
+  holder = []
+  chars_array.each_with_index do |char, index|
+    i = index + 1
+    while i < chars_size
+      holder << chars_array[index..i]
+      i += 1
+    end
+  end
+  holder.map do |string|
+    string.join
+  end
 end
+
+def palindromes(str)
+  array_of_strings = substrings(str)
+  array_of_strings.select do |string|
+    string == string.reverse
+  end
+end
+
+
+
+# Write a method that returns a list of all substrings of a string that are
+# palindromic.
 
 
 puts palindromes('abcd') == []
@@ -12,3 +37,16 @@ puts palindromes('hello-madam-did-madam-goodbye') == [
 puts palindromes('knitting cassettes') == [
   'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
 ]
+
+
+# Method
+
+# input = string, output = array of strings.
+
+# Algorithm
+
+# 1. Get all 'greater than 1 char' substrings from a string. (write a method substrings for this)
+  # Iterate through the string, and at each iteration, implement a loop.
+  # The loop
+# 2. Find out which ones are palindromes.
+# 3. Return the ones that are palindromes.
