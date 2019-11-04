a8b5e9cae2296bdac176b18a1b14277cb9b73d72
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-03 22:30:56 -0800

    understanding what a transpose is

---
+++ transpose_3x3.rb
@@ -0,0 +1,19 @@
+def transpose(mtx)
+  copy_mtx = mtx.clone
+
+end
+
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+
+
+# The transpose of a matrix is a new matrix whose rows are the columns of the original. 
